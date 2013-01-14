f140235a3b789fe1981bcb93a45db516330d7bf4
Christopher Imrie <chris@moresoda.co.uk>
2013-01-14 13:06:48 +0000

    Added english language file

---
+++ language/english/lang.module_name.php
@@ -0,0 +1,19 @@
+<?php if (! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+require_once '../../config.php';
+
+$lang = array(
+
+	/**
+	 * Module Name & Description
+	 */
+	'THIS_MODULE_module_name' => THIS_MODULE_NAME,
+	'THIS_MODULE_module_description' => THIS_MODULE_DESC
+
+	/**
+	 * Other language Keys
+	 */
+	
+	// "key" => "Value"
+);
